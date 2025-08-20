6ed17b72efe7e4f5dffd13919347ed4f919e1161
signor1 <emmanuelomemgboji@gmail.com>
2025-08-20 19:01:40 +0100

    feat: test

--- contracts/management-system/src/test.rs
+++ contracts/management-system/src/test.rs
@@ -264,3 +264,107 @@ fn test_remove_employee() {
 
     assert_eq!(institution_info.total_employees, 0);
 }
+
+#[test]
+fn test_remove_employee_not_found() {
+    let (env, admin, employee1, _, token_contract) = setup_test();
+    let contract_id = env.register(EmployeeManagementContract, ());
+    let mgt_client = EmployeeManagementContractClient::new(&env, &contract_id);
+
+    let result = mgt_client.initialize(
+        &admin.clone(),
+        &String::from_str(&env, "QA Institution"),
+        &token_contract.clone(),
+    );
+
+    assert_eq!(result, ());
+
+    // Remove employee
+    let result = mgt_client.try_remove_employee(&admin, &employee1);
+
+    assert_eq!(
+        result.unwrap_err(),
+        Ok(ContractError::EmployeeNotFound),
+        "Error should be EmployeeNotFound"
+    );
+
+    let institution_info = mgt_client.get_institution_info();
+
+    assert_eq!(institution_info.total_employees, 0);
+}
+
+#[test]
+fn test_update_employee() {
+    let (env, admin, employee1, _, token_contract) = setup_test();
+    let contract_id = env.register(EmployeeManagementContract, ());
+    let mgt_client = EmployeeManagementContractClient::new(&env, &contract_id);
+
+    let result = mgt_client.initialize(
+        &admin.clone(),
+        &String::from_str(&env, "QA Institution"),
+        &token_contract.clone(),
+    );
+
+    assert_eq!(result, ());
+
+    // Add first employee
+    mgt_client.add_employee(
+        &admin,
+        &employee1,
+        &String::from_str(&env, "John Doe"),
+        &1000,
+        &EmployeeRank::Junior,
+    );
+
+    // Update employee
+    let result = mgt_client.update_employee(
+        &admin,
+        &employee1,
+        &Some(String::from_str(&env, "Jane Linda")),
+        &Some(2000),
+    );
+
+    assert_eq!(result, ());
+
+    let employee_info = mgt_client.get_employee(&employee1);
+
+    assert_eq!(employee_info.name, String::from_str(&env, "Jane Linda"));
+    assert_eq!(employee_info.salary, 2000);
+
+    let institution_info = mgt_client.get_institution_info();
+
+    assert_eq!(institution_info.total_employees, 1);
+}
+
+#[test]
+fn test_update_employee_not_found() {
+    let (env, admin, employee1, _, token_contract) = setup_test();
+    let contract_id = env.register(EmployeeManagementContract, ());
+    let mgt_client = EmployeeManagementContractClient::new(&env, &contract_id);
+
+    let result = mgt_client.initialize(
+        &admin.clone(),
+        &String::from_str(&env, "QA Institution"),
+        &token_contract.clone(),
+    );
+
+    assert_eq!(result, ());
+
+    // Update employee
+    let result = mgt_client.try_update_employee(
+        &admin,
+        &employee1,
+        &Some(String::from_str(&env, "Jane Linda")),
+        &Some(2000),
+    );
+
+    assert_eq!(
+        result.unwrap_err(),
+        Ok(ContractError::EmployeeNotFound),
+        "Error should be EmployeeNotFound"
+    );
+
+    let institution_info = mgt_client.get_institution_info();
+
+    assert_eq!(institution_info.total_employees, 0);
+}
